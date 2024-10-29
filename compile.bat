8212b703ebf700ed3dc64fd6a3f60e8dd2935d9b
Goshoon <treskks@gmail.com>
2024-10-29 09:41:42 -0700

    Compilador

--- compile.bat
+++ compile.bat
@@ -1,22 +1,27 @@
 @echo off
-
-:: enabledelayedexpansion permite utilizar !SOURCES! que es la manera de acceder una variable dentro de un ciclo
 setlocal enabledelayedexpansion
 set "SOURCES="
+set "OBJECTS="
 set "INCLUDES="
 
-:: Buscar todos los archivos .cpp dentro de ./Source y carpetas dentro de ./Source/
+:: Collect all source files and their respective object file paths
 for /R ./Source %%f in (*.cpp) do (
     set "SOURCES=!SOURCES! %%f"
+    set "OBJECTS=!OBJECTS! %%~nf.o"
 )
 
-:: Buscar todas las carpetas de inclusion dentro de ./Headers/
+:: Collect all include directories
 for /R ./Headers /D %%d in (*) do (
     set "INCLUDES=!INCLUDES! %%d"
 )
 
-:: Compilar
-g++ -std=c++17 %SOURCES% -I ./Headers/ -I %INCLUDES% -I ./Dependencies/SDL2/include -I ./Dependencies/ImGui -L ./Dependencies/SDL2/lib -o ./Bin/run.exe -lmingw32 -lSDL2main -lSDL2 -lSDL2_image
+:: Compile each source file into an object file
+for %%f in (%SOURCES%) do (
+    g++ -std=c++17 -c %%f -I ./Headers/ -I %INCLUDES% -I ./Dependencies/SDL2/include -I ./Dependencies/ImGui -o %%~nf.o
+)
+
+:: Link all object files into the final executable
+g++ %OBJECTS% -L ./Dependencies/SDL2/lib -o ./Bin/run.exe -lmingw32 -lSDL2main -lSDL2 -lSDL2_image
 
 endlocal
 pause
